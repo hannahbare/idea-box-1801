4ea119c7e6fb048d33f3a50d1142e33b5dd86a76
Hannah Bare <johnd>
2018-02-10 10:35:14 -0700

    Add captureVal function

--- script.js
+++ script.js
@@ -2,10 +2,27 @@ var $userInputTitle = $('#userinput__title');
 var $userInputBody = $('#userinput__body');
 var $saveBtn = $('#userinput__btn');
 
-function card(id, title, body, quality){
-  this.id = id;
-  this.title = title;
-  this.body = body;
-  this.quality = 'swill' || quality;
+
+$saveBtn.on('click', captureVals)
+
+
+function captureVals(){
+  var $userTitleVal = $userInputTitle.val();
+  var $userInputVal = $userInputBody.val();
+  
 }
+
+
+  // var newCard = new Card(id, $userTitleVal, $userInputBody, quality);
+
+
+
+
+
+// function Card(id, title, body, quality){
+//   this.id = id;
+//   this.title = title;
+//   this.body = body;
+//   this.quality = 'swill' || quality;
+// }
 
