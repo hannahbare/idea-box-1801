a2b97cfe96002d95d0819c51d49cd0bc9743a245
Hannah Bare <johnd>
2018-02-12 16:50:10 -0700

    Key value pairs now store in localStorage

--- script.js
+++ script.js
@@ -2,15 +2,26 @@ var $userInputTitle = $('#userinput__title');
 var $userInputBody = $('#userinput__body');
 var $saveBtn = $('#userinput__btn');
 
+$saveBtn.on('click', newInstance);
 
-$saveBtn.on('click', appendCard);
-$('card__icon--upvote').on('click', toggleUpvote);
-$('card__icon--downvote').on('click', toggleDownvote);
+function Card(){
+  this.id = Date.now();
+  this.title = $userInputTitle.val();
+  this.body = $userInputBody.val();
+  this.quality = 'Swill';
+}
 
-function appendCard(){
-  var newCard = new Card;
+function newInstance(e){
+  e.preventDefault();
+  var newCard = new Card();
+  appendCard(newCard);
+  storeCard(newCard.id, newCard);
+  clearInputs();
+}
+
+function appendCard(newCard) {
   var $section = $('.card__list');
-  $section.prepend(`<article>
+  $section.prepend(`<article id="${newCard.id}">
       <h2 contenteditable="false">${newCard.title}</h2>
       <img src="assets/delete.svg" class="card__icon" id="card__icon--delete">
       <p contenteditable="false">${newCard.body}</p>
@@ -20,50 +31,40 @@ function appendCard(){
       </div>
       <h6>quality: ${newCard.quality}</h6>
     </article>`);
-  clearInputs();
+  console.log(newCard.id)
+}
+
+function storeCard(id, newCard) {
+  var stringifiedNewCard = JSON.stringify(newCard);
+  var stringifiedId = JSON.stringify(id);
+  localStorage.setItem(stringifiedId, stringifiedNewCard);
+};
+
+function myStore(string1, string2){
+  localStorage.setItem(string1, string2);
 }
 
 function clearInputs(){
   $userInputTitle.val('');
   $userInputBody.val('');
 }
-c
-  // var newCard = new Card(date.now(), $userTitleVal, $userBodyVal);
-//create a new instance- capturing the id (JSON)
 
-function Card(id, title, body){
-  this.id = id;
-  this.title = $userInputTitle.val();
-  this.body = $userInputBody.val();
-  this.quality = 'Swill';
-}
+// Card.prototype.toggleUpvote = function(quality){
+//   //hey card-- i need you. now I need to see your quality and let's change it
+//   if(quality === 'Swill'){
+//       this.quality = 'Plausible';
+//   } else if (this.quality === 'Plausible'){
+//     this.quality = 'Genius';
+//   }
+// }
 
-Card.prototype.toggleUpvote = function(){
-  // console.log('&userBodyVal');
-  if(this.quality === 'Swill'){
-      this.quality = 'Plausible';
-  } else if (this.quality === 'Plausible'){
-    this.quality = 'Genius';
-  }
-}
-
-Card.prototype.toggleDownvote = function(){
-    if(this.quality === 'Genius'){
-      this.quality = 'Plausible';
-  } else if (this.quality === 'Plausible'){
-    this.quality = 'Swill';
-  }
-}
-
-
-
-
-
-
-
-
-
-
+// Card.prototype.toggleDownvote = function(){
+//     if(this.quality === 'Genius'){
+//       this.quality = 'Plausible';
+//   } else if (this.quality === 'Plausible'){
+//     this.quality = 'Swill';
+//   }
+// }
 
 
 
